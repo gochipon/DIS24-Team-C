7d091ee7d30d6a4386f5f7017ad78ed3e7a6cb8c
iamkimam <103396285+iamkimam@users.noreply.github.com>
2024-08-09 15:44:47 +0900

    pc2txt version2

--- backend/pc2txt/from_pc.py
+++ backend/pc2txt/from_pc.py
@@ -2,18 +2,16 @@ from pinecone.grpc import PineconeGRPC as Pinecone_api
 from langchain_openai import OpenAIEmbeddings 
 from langchain_pinecone import PineconeVectorStore  
 from langchain_openai import ChatOpenAI  
-from langchain.chains import RetrievalQA  
-import time  
+# from langchain.chains import RetrievalQA  
+# import time  
 import os
 
-def main(query: str, topk: int) -> None:  
+def main(query: str, topk: int, index_name: str, repository: str) -> None:  
     # configure client  
     PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
     OPEN_AI_API_KEY = os.environ['OPENAI_API_KEY']
 
     pc = Pinecone_api(api_key=PINECONE_API_KEY)  
-
-    index_name = 'cli-cli'
     index = pc.Index(index_name)  
 
     model_name = 'text-embedding-ada-002'  
@@ -29,25 +27,53 @@ def main(query: str, topk: int) -> None:
         text_field  
     )
 
-    llm = ChatOpenAI(  
-        openai_api_key=OPEN_AI_API_KEY,  
-        model_name='gpt-3.5-turbo',  
-        temperature=0.0  
-    )  
-
-    qa = RetrievalQA.from_chain_type(  
-        llm=llm,  
-        chain_type="stuff",  
-        retriever=vectorstore.as_retriever()  
-    )  
-
-    vectorstore.similarity_search_with_relevance_scores(  
+    top_k_data = vectorstore.similarity_search_with_relevance_scores(  
         query,  # our search query  
-        k=topk  # return 3 most relevant docs  
+        k=topk,  # return 3 most relevant docs  
+        filter={
+            'repository':repository,
+            # '_ab_stream':'releases'
+        },
     )  
 
-    print(qa.run(query))
+    top_k_id = []
 
+    if top_k_data:
+        for k_data in top_k_data:
+            
+            info={}
+            meta= k_data[0].metadata
+            stream = meta['_ab_stream']
+            info['repository'] = meta['repository']
+
+            info['socre'] = k_data[1]
+            
+            if stream == 'releases':
+                info['name'] = stream
+                info['id'] = meta['name']
+                
+
+            elif stream == 'comments':
+                info['name'] = 'issues'
+                info['id'] = meta['issue_url'].split('/')[-1]        
+            elif stream == 'issues':
+                info['name'] = stream
+                info['id'] = meta['number']
+
+            elif stream == 'pull_requests':
+                info['name'] = stream
+                info['id'] = meta['number']
+            elif stream == 'review_comments':
+                info['name'] = 'pull_requests'
+                info['id'] = meta['pull_request_url'].split('/')[-1]
+            else:
+                continue
+        
+            top_k_id.append(info)
+
+    print(top_k_id)
+    
 if __name__ == "__main__":  
     query = "Tell me information about `gh repo view` on forked repositories."
-    main(query, topk=3)
+    repository = "cli/cli"
+    main(query, topk=5, index_name='cli-cli-1', repository=repository)
