9902eeeec52a129b77979b3e4d120df83bed5cfe
Shion Ichikawa <shion1305@gmail.com>
2024-08-10 08:56:10 +0900

    ✨ ENV_CONTENT

--- backend/pkg/config/config.go
+++ backend/pkg/config/config.go
@@ -20,6 +20,26 @@ var C *Config
 
 func init() {
 	godotenv.Load()
+	if os.Getenv("ENV_CONTENT") != "" {
+		envs, err := godotenv.Unmarshal(os.Getenv("ENV_CONTENT"))
+		if err != nil {
+			panic(err)
+		}
+		C = &Config{
+			DBHost:        envs["DB_HOST"],
+			DBPort:        envs["DB_PORT"],
+			DBUser:        envs["DB_USER"],
+			DBPassword:    envs["DB_PASSWORD"],
+			DBName:        envs["DB_NAME"],
+			PineconeKey:   envs["PINECONE_KEY"],
+			OpenaiKey:     envs["OPENAI_KEY"],
+			PineconeIndex: envs["PINECONE_INDEX"],
+		}
+		if C.DBHost == "" || C.DBPort == "" || C.DBUser == "" || C.DBPassword == "" || C.DBName == "" || C.PineconeKey == "" || C.OpenaiKey == "" || C.PineconeIndex == "" {
+			panic("missing env var")
+		}
+		return
+	}
 	C = &Config{
 		DBHost:        getEnv("DB_HOST"),
 		DBPort:        getEnv("DB_PORT"),
