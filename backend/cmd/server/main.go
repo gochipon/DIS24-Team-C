ea2406f9c18432a36d41769d30f9b9bcdc2ec910
Shion Ichikawa <shion1305@gmail.com>
2024-08-09 18:40:36 +0900

    🚧 server complete, options request

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gochipon/DIS24-Team-C/backend/pkg/handler"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main() {
 	e := gin.Default()
-	h := handler.NewInfoHandler()
+	godotenv.Load()
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+		return
+	}
+	h := handler.NewInfoHandler(db)
 	// /:org/:repo/issues/:number
 	e.Use(func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
@@ -16,8 +32,12 @@ func main() {
 	})
 	e.GET("/api/v1/:org/:repo/issue/:number", h.Issue())
 	e.GET("/api/v1/:org/:repo/pull/:number", h.PullRequest())
-	err := e.Run(":8080")
-	if err != nil {
+	e.NoRoute(func(c *gin.Context) {
+		if c.Request.Method == "OPTIONS" {
+			c.JSON(200, "Options Request!")
+		}
+	})
+	if err := e.Run(":8080"); err != nil {
 		return
 	}
 }
