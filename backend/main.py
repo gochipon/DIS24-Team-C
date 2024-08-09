ccbb37188a13d9130ec9a977e4809cbf55e3bab5
HashimotoLF <103479971+HashimotoLF@users.noreply.github.com>
2024-08-09 22:00:04 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import json
 import modules.SearchFromVDB as vectorsearch
 import modules.SearchFromProgreSQL as itemsearch
 import modules.Summarizer as summarizer
+import modules.RelatedPartFinder as relatedpartfinder
 
 # set query
 query = "fix a bug"
@@ -14,10 +15,5 @@ index_name = "cli-cli-1"
 vecsearch_results = vectorsearch.main(query, topk, index_name, repository)
 # sql search
 sql_query = itemsearch.main(vecsearch_results)
-# extract body from sql query
-body_list = [x['body'] for x in sql_query]
-# merge saerch results
-merge_result = "\n".join(body_list)
-# summarize
-summary = summarizer.main(merge_result)
-print(summary)
+result = [{"title": _res["title"], "url": _res["url"], "summary": summarizer.main(_res['body']), "related_part": relatedpartfinder.main(query, _res["body"])} for _res in sql_query]
+print(result)
