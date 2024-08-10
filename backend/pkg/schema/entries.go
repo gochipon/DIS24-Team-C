523125c255960d9ff2070b7e9721a149566a56c7
Shion Ichikawa <shion1305@gmail.com>
2024-08-10 09:17:40 +0900

    🐛 Summary

--- backend/pkg/schema/entries.go
+++ backend/pkg/schema/entries.go
@@ -3,5 +3,6 @@ package schema
 type SearchEntry struct {
 	Type    string      `json:"type"`
 	Score   float32     `json:"score"`
+	Summary string      `json:"summary"`
 	Content interface{} `json:"content"`
 }
