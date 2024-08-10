ae50d563d69ddae2da25eeafe7434d00b6ee3b52
Shion Ichikawa <shion1305@gmail.com>
2024-08-10 15:45:43 +0900

    🔊

--- gh-app/issue-check.go
+++ gh-app/issue-check.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/sashabaranov/go-openai"
+	"log"
 	"regexp"
 	"strconv"
 )
@@ -26,7 +27,7 @@ func (s *IssueCheck) CheckIssue(title, body string) int {
 	ctx := context.Background()
 	resp, err := s.client.CreateChatCompletion(ctx,
 		openai.ChatCompletionRequest{
-			Model: "gpt-3.5-turbo",
+			Model: "gpt-4o",
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    "system",
@@ -34,13 +35,14 @@ func (s *IssueCheck) CheckIssue(title, body string) int {
 				},
 				{
 					Role:    "user",
-					Content: "次に与えられるGithubのissueを、'内容が十分'、'内容が不十分'、'スパム'のいずれかに分類してください。以下の手順に従ってください \n 1. 与えられたGithubのissueを注意深く読み、内容を理解します。\n 2. 内容が十分であるかどうかを判断します。十分な情報が含まれている場合は、'内容が十分'と分類します。\n 3. 情報が不足している場合や、質問が不明確な場合は、'内容が不十分'と分類します。 \n 4. 明らかにスパムである場合（例えば、無関係なリンクや広告、意味をなさない文章など）は、'スパム'と分類します。\n 5. 出力結果は以下のフォーマットに従ってください。出力にはXMLタグを含めないでください。 \n '内容が十分'の場合: 0 '内容が不十分'の場合:1 \n 'スパム'の場合: 2 \n 与えられるissueは以下の通りです。\n\n" + "Title: " + title + "\n\n" + "Body: " + body,
+					Content: "次に与えられるGithubのissueを、'内容が十分'、'内容が不十分'、'スパム'のいずれかに分類してください。以下の手順に従ってください \n 1. 与えられたGithubのissueを注意深く読み、内容を理解します。\n 2. 内容が十分であるかどうかを判断します。十分な情報が含まれている場合は、'内容が十分'と分類します。\n 3. 情報が不足している場合や、質問が不明確な場合は、'内容が不十分'と分類します。 \n 4. 明らかにスパムである場合（例えば、無関係なリンクや広告、意味をなさない文章やIssueとして適切でないものなど）は、'スパム'と分類します。\n 5. 出力結果は以下のフォーマットに従ってください。出力にはXMLタグを含めないでください。 \n '内容が十分'の場合: 0 '内容が不十分'の場合:1 \n 'スパム'の場合: 2 \n 与えられるissueは以下の通りです。\n\n---" + "Title: " + title + "\n\n" + "Body: " + body,
 				},
 			},
 		})
 	if err != nil {
 		return 0
 	}
+	log.Println(resp.Choices[0].Message.Content)
 	return extractLastNumber(resp.Choices[0].Message.Content)
 	// extract number from response
 }
